91ad8db5223622a97569f949ce52dcf810769ca6
unknown <piperin@byu.edu>
2026-03-17 17:58:37 -0600

    Fix linting issue x2

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -85,7 +85,8 @@ authRouter.put(
       res.json({ user: user, token: auth });
     } catch (err) {
       metrics.trackAuth(false);
-      return res.status(401).json({ message: err, ': invalid email or password' });
+      console.error('Auth error:', err);
+      return res.status(401).json({ message: ': invalid email or password' });
     }
   })
 );
