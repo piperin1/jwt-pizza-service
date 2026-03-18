d0b165e545918f97f5d9b60298522dfdfb2db055
unknown <piperin@byu.edu>
2026-03-17 18:00:18 -0600

    Fix test issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const app = require('./service.js');
 const metrics = require('./metrics.js');
-app.use(metrics.requestTracker);
+if(app.use){
+  app.use(metrics.requestTracker);
+}
 metrics.sendMetricsPeriodically(60000);
 const port = process.argv[2] || 3000;
 app.listen(port, () => {
