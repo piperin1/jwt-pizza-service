fd97fdd3bf9f2a908261dd1ef52eda510a17906c
unknown <piperin@byu.edu>
2026-03-18 18:30:23 -0600

    change aggregation

--- src/metrics.js
+++ src/metrics.js
@@ -63,7 +63,7 @@ function sendMetricToGrafana(metricName, metricValue, type, unit) {
 
   if (type === 'sum') {
     metric.resourceMetrics[0].scopeMetrics[0].metrics[0][type].aggregationTemporality =
-      'AGGREGATION_TEMPORALITY_CUMULATIVE';
+      'AGGREGATION_TEMPORALITY_DELTA';
     metric.resourceMetrics[0].scopeMetrics[0].metrics[0][type].isMonotonic = true;
   }
   
