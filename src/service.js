410896bf2c857da0d32669a48c0d559ea1ffab85
unknown <piperin@byu.edu>
2026-03-23 18:42:37 -0600

    Fix linting error

--- src/service.js
+++ src/service.js
@@ -54,7 +54,7 @@ app.use('*', (req, res) => {
 });
 
 // Default error handler for all exceptions and errors.
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   logError(err);
   res.status(err.statusCode ?? 500).json({
     message: err.message,
